8a2aa1f70476d9752ed4dd1201fb3b86f837096e
Louis Huy Nguyen <huynguyennbk@gmail.com>
2024-09-02 22:48:42 +0700

    create subscription application usecase

--- src/GymManagement.Api/Controllers/SubscriptionsController.cs
+++ src/GymManagement.Api/Controllers/SubscriptionsController.cs
@@ -1,4 +1,5 @@
 using GymManagement.Contracts.Subscriptions;
+using GymManagement.Application.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GymManagement.Api.Controllers;
@@ -7,9 +8,22 @@ namespace GymManagement.Api.Controllers;
 [Route("[controller]")]
 public class SubscriptionsController : ControllerBase
 {
+  private readonly ISubscriptionsService _subscriptionsService;
+
+  public SubscriptionsController(ISubscriptionsService subscriptionsService)
+  {
+    _subscriptionsService = subscriptionsService;
+  }
+
   [HttpPost]
   public IActionResult CreateSubscription(CreateSubscriptionRequest request)
   {
-    return Ok(request);
+    var subscriptionId = _subscriptionsService.CreateSubscription(
+        request.SubscriptionType.ToString(), request.AdminId
+        );
+
+    var response = new SubscriptionResponse(subscriptionId, request.SubscriptionType);
+
+    return Ok(response);
   }
 }
