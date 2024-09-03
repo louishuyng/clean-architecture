6913eb80d19029d139295cecbb9496b5e555fd50
Louis Huy Nguyen <huynguyennbk@gmail.com>
2024-09-03 20:40:00 +0700

    apply result pattern inside application and using match on presentation layer to response back

--- src/GymManagement.Api/Controllers/SubscriptionsController.cs
+++ src/GymManagement.Api/Controllers/SubscriptionsController.cs
@@ -24,10 +24,11 @@ public class SubscriptionsController : ControllerBase
         request.AdminId
     );
 
-    var subscriptionId = await _mediator.Send(command);
+    var createSubscriptionResult = await _mediator.Send(command);
 
-    var response = new SubscriptionResponse(subscriptionId, request.SubscriptionType);
-
-    return Ok(response);
+    return createSubscriptionResult.MatchFirst(
+        guid => Ok(new SubscriptionResponse(guid, request.SubscriptionType)),
+        error => Problem()
+    );
   }
 }
