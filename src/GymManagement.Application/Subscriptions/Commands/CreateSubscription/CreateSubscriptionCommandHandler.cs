efba91bf99ca79d5352947e692d332f32ba2dc28
Louis Huy Nguyen <huynguyennbk@gmail.com>
2024-09-05 19:13:09 +0700

    using unit of work from db context

--- src/GymManagement.Application/Subscriptions/Commands/CreateSubscription/CreateSubscriptionCommandHandler.cs
+++ src/GymManagement.Application/Subscriptions/Commands/CreateSubscription/CreateSubscriptionCommandHandler.cs
@@ -8,12 +8,12 @@ namespace GymManagement.Application.Subscriptions.Commands.CreateSubscription;
 public class CreateSubscriptionCommandHandler : IRequestHandler<CreateSubscriptionCommand, ErrorOr<Subscription>>
 {
   private readonly ISubscriptionsRepository _subscriptionsRepository;
-  // private readonly IUnitOfWork _unitOfWork;
+  private readonly IUnitOfWork _unitOfWork;
 
-  public CreateSubscriptionCommandHandler(ISubscriptionsRepository subscriptionsRepository)
+  public CreateSubscriptionCommandHandler(ISubscriptionsRepository subscriptionsRepository, IUnitOfWork unitOfWork)
   {
     _subscriptionsRepository = subscriptionsRepository;
-    // _unitOfWork = unitOfWork;
+    _unitOfWork = unitOfWork;
   }
 
   public async Task<ErrorOr<Subscription>> Handle(CreateSubscriptionCommand request, CancellationToken cancellationToken)
@@ -27,7 +27,7 @@ public class CreateSubscriptionCommandHandler : IRequestHandler<CreateSubscripti
 
     // Add it to a database
     await _subscriptionsRepository.AddSubscriptionAsync(subscription);
-    // await _unitOfWork.CommitChangesAsync();
+    await _unitOfWork.CommitChangesAsync();
 
     // Return subscription
     return subscription;
--- src/GymManagement.Infrastructure/DependencyInjection.cs
+++ src/GymManagement.Infrastructure/DependencyInjection.cs
@@ -14,6 +14,7 @@ public static class DependencyInjection
     services.AddDbContext<GymManagementDbContext>(options => options.UseSqlite("Data Source = GymManagement.db"));
 
     services.AddScoped<ISubscriptionsRepository, SubscriptionsRepository>();
+    services.AddScoped<IUnitOfWork>(serviceProvider => serviceProvider.GetRequiredService<GymManagementDbContext>());
 
     return services;
   }
